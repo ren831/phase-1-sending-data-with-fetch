bf33ed4e7c042a16bb3b46b987c9b08ee8455031
Rene Hernandez <rene83120@gmail.com>
2022-02-23 14:00:49 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(userName, userEmail) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: userName,
+      email: userEmail,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((body) => addNewContact(body.id))
+    .catch(function (error) {
+      alert("Unauthorized Acess");
+      document.body.innerHTML = error.message;
+    });
+}
+
+function addNewContact(newID) {
+  document.getElementsByTagName("body")[0].innerHTML = newID;
+}
